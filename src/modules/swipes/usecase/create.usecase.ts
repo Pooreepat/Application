847b0becc657b66938aaed243a4ff1270995e9b8
Denmann N'ramzz <116389026+TxngJr@users.noreply.github.com>
2024-10-08 03:31:41 +0700

    remove comment

--- src/modules/swipes/usecase/create.usecase.ts
+++ src/modules/swipes/usecase/create.usecase.ts
@@ -6,6 +6,7 @@ import { SwipeService } from '../swipes.service';
 import { SwipeCreateDto } from '../dto/swipes-create.dto';
 import { MatchService } from 'src/modules/matches/matches.service';
 import { MatchStatus } from 'src/modules/matches/matches.constant';
+import { SwipeType } from '../swipes.constant';
 
 @Injectable()
 export class CreateSwipesUsecase {
@@ -19,42 +20,40 @@ export class CreateSwipesUsecase {
     data: SwipeCreateDto & { _swiperId: Types.ObjectId },
   ): Promise<HttpRespons> {
     try {
-      // Ensure _swipedPetId and _swipedId are converted to ObjectId
       const _swipedPetId = new Types.ObjectId(data._swipedPetId);
       const _swipedId = new Types.ObjectId(data._swipedId);
 
-      // Check if the swipe already exists
       const checkSwipe = await this.swipeService.findByFilter({
         _swiperId: data._swiperId,
-        _swipedPetId: _swipedPetId, // Use ObjectId
-        _swipedId: _swipedId, // Use ObjectId
+        _swipedPetId: _swipedPetId,
+        _swipedId: _swipedId,
       });
 
       if (checkSwipe) {
         throw new HttpException('สัตว์เลี้ยงนี้ถูกปั้นแล้ว', 500);
       }
 
-      // Create a new swipe with ObjectId conversion
       const swipe = await this.swipeService.create({
         ...data,
-        _swipedPetId: _swipedPetId, // Ensure ObjectId is used
-        _swipedId: _swipedId, // Ensure ObjectId is used
+        _swipedPetId: _swipedPetId,
+        _swipedId: _swipedId,
       });
 
       if (!swipe) {
         throw new HttpException('ไม่สามารถปั้นสัตว์เลี้ยงได้', 500);
       }
 
-      // Create a match entry after swipe
-      const match = await this.matchesService.create({
-        _profile1Id: swipe._swiperId,
-        _profile2Id: _swipedId, // Use ObjectId
-        _petId: _swipedPetId, // Use ObjectId
-        status: MatchStatus.UNMATCHED,
-      });
+      if (swipe.swipeType == SwipeType.LIKE) {
+        const match = await this.matchesService.create({
+          _profile1Id: swipe._swiperId,
+          _profile2Id: _swipedId,
+          _petId: _swipedPetId,
+          status: MatchStatus.UNMATCHED,
+        });
 
-      if (!match) {
-        throw new HttpException('ไม่สามารถสร้างแมตช์ได้', 500);
+        if (!match) {
+          throw new HttpException('ไม่สามารถสร้างแมตช์ได้', 500);
+        }
       }
 
       return {
