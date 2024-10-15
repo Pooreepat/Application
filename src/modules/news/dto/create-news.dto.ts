4bbaac18c51975a9225e8431a0b95219c48e63e9
Denmann N'ramzz <116389026+TxngJr@users.noreply.github.com>
2024-10-15 16:34:26 +0700

    add

--- src/modules/news/dto/create-news.dto.ts
+++ src/modules/news/dto/create-news.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
+import { IsString, IsNotEmpty, IsEnum, IsArray } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { ENewsType } from '../news.constant';
 
@@ -12,6 +12,16 @@ export class CreateNewsDto {
   @IsString()
   @IsNotEmpty()
   content: string;
+
+  @ApiProperty({
+    description: 'Images of the Pet',
+    type: [String],
+    example: ['image1.jpg', 'image2.jpg'],
+  })
+  @IsNotEmpty()
+  @IsArray()
+  @IsString({ each: true })
+  images: string[];
 
   @ApiProperty({
     description: 'Type of the news',
